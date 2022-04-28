7dc5bc89a6ecb54788f7c2343f352c89d94e23f8
aidanliddiard <aidanliddiard@gmail.com>
2022-04-28 16:39:31 -0700

    search is working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function SearchBar() {
-  const handleSearch = async () => {
-    console.log('click');
-  };
+export default function SearchBar({ setQuery }) {
+  const [searching, setSearching] = useState('');
+
   return (
     <div>
-      <input></input>
-      <button onClick={handleSearch}>Search Quotes</button>
+      <input onChange={(e) => setSearching(e.target.value)}></input>
+      <button name="searching" onClick={() => setQuery(searching)}>
+        Search Quotes
+      </button>
     </div>
   );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import getQuotes from '../services/futurama';
+import getQuotes, { searchQuotes } from '../services/futurama';
 import QuoteCard from '../components/QuoteCard';
 import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     try {
       const fetchData = async () => {
         const data = await getQuotes();
         setQuotes(data);
+        const searchData = await searchQuotes(query);
+        setQuotes(searchData);
         setLoading(false);
       };
       fetchData();
     } catch (error) {
       console.error(error.message);
     }
-  }, []);
+  }, [query]);
+
   if (loading) return <p>Loading...</p>;
+  //console.log(query);
 
   return (
     <div>
-      <SearchBar />
+      <SearchBar setQuery={setQuery} />
       {quotes.map((quote) => (
         <QuoteCard key={quote.quote} {...quote} />
       ))}
